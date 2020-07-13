82db3494912592641078b053909a069af4b428ac
yudadev <daniel.yu.webdev@gmail.com>
2020-07-12 22:52:58 -0400

    disable menu item for corresponding current route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
 	selector: 'app-root',
@@ -9,6 +10,7 @@ export class AppComponent implements OnInit {
 	title = 'angular';
 
 	constructor(
+		public router: Router
 	) {
 
 	}
